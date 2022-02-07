ca655f483665d74f49c8a780b8f5fe9968eec401
Harald Lilja <harald.lilja@prevas.se>
2022-02-07 22:22:43 +0100

    added tmux-ssh.sh to ssh to known ssh host

---
+++ bin/.local/bin/tmux-ssh.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+hosts=`grep -P "^Host ([^*]+)$" $HOME/.ssh/config | sed 's/Host //'`
+
+host=`printf "$hosts" | fzf | awk '{print $1}'` # pick first name
+tmux neww bash -c "sshpass -f ~/ssh_pass ssh $host"
