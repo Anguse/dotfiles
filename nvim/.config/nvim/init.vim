5fa11b4e55b1ecfdec74927a81c62a6caad6f68a
Harald Lilja <liljaharald@gmail.com>
2023-09-04 01:05:53 +0200

    Use primeage lsp config

--- nvim/.config/nvim/init.vim
+++ nvim/.config/nvim/init.vim
@@ -3,7 +3,8 @@ source ~/.config/nvim/init/functions.vimrc
 source ~/.config/nvim/init/plugins.vimrc
 
 lua require "raldo.colorscheme"
-lua require "raldo.packer"
 lua require "raldo.set"
-lua require "raldo.cmp"
-lua require "raldo.lsp"
+lua require "raldo.remap"
+lua require "raldo.packer"
+" lua require "raldo.cmp"
+" lua require "raldo.lsp"
--- nvim/.config/nvim/lua/raldo/packer.lua
+++ nvim/.config/nvim/lua/raldo/packer.lua
@@ -26,16 +26,28 @@ return require('packer').startup(function()
 
     -- LSP
     use {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig",
-        "jose-elias-alvarez/null-ls.nvim"
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        }
     }
     use 'nvim-treesitter/nvim-treesitter'
-    use 'hrsh7th/nvim-cmp'
-    use "hrsh7th/cmp-nvim-lsp"
-    use "saadparwaiz1/cmp_luasnip" -- snippet completions
-    use "hrsh7th/cmp-cmdline"
 
     -- Diagnostics
     use {
