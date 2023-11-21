859fff35f3b3b04a3744907abd9edc9abc6eb46b
Harald Lilja <harald.lilja@getinge.com>
2023-11-21 15:08:40 +0100

    Add which-key

---
+++ nvim/.config/nvim/after/plugin/which-key.lua
@@ -0,0 +1,6 @@
+local wk = require("which-key")
+wk.register({
+	c = {
+		c = { "<cmd>ChatGPT<cr>", "ChatGPT" }
+	}
+}, { prefix = "<leader>" })
--- nvim/.config/nvim/lua/raldo/packer.lua
+++ nvim/.config/nvim/lua/raldo/packer.lua
@@ -101,6 +101,20 @@ return require('packer').startup(function()
       requires = {'nvim-tree/nvim-web-devicons'}
     }
 
+    -- Keybindings
+    use {
+      "folke/which-key.nvim",
+      config = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+        require("which-key").setup {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+        }
+      end
+    }
+
     -- Git
     use 'tpope/vim-fugitive'
     use 'tpope/vim-rhubarb'
